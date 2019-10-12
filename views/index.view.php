ecf9a29e7fde63f1c058691123d496d1caedb8b5
Stephane Dzefo <fredicm25@gmail.com>
2019-10-12 14:10:45 +0100

    initialisation du porjet

---
+++ views/index.view.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="reseau socail ">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Social Network</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="assets/css/main.css" rel="stylesheet" >
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <a class="navbar-brand" href="#">Social Network</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Inscription</a>
+      </li>
+    </ul>
+  
+  </div>
+</nav>
+
+<main role="main" class="container">
+  <div class="jumbotron">
+    <h1>Qu'est-ce que Social Network?</h1>
+    <p class="lead">
+
+    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
+    Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500<br>
+    pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, 
+    sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de 
+    feuilles Letraset contenant des passages du Lorem Ipsum, et,
+     plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+    <a class="btn btn-lg btn-primary" href="inscription.php" role="button">je m'inscris &raquo;</a>
+  </div>
+</main>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js" ></script>
+    <script src="assets/js/main.js" ></script>
+</body>
+</html>
