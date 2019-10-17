d5cbee3eb453285e888d70792a026ec5668b38d2
Stephane Dzefo <fredicm25@gmail.com>
2019-10-17 07:56:05 +0100

    affichage des infomation de l'utilisatuer sur on profil

--- views/profil.view.php
+++ views/profil.view.php
@@ -28,13 +28,42 @@
         <div class="row"> <!--debut de la ligne  -->
             <div class="col-md-6">
                 <strong><?= e($user->pseudo) ?></strong><br>
-               <a href="mailto:<?= e($user->email) ?>"><strong><?= e($user->email) ?></strong></a> 
+               <a href="mailto:<?= e($user->email) ?>"><strong><?= e($user->email) ?></strong></a> <br>
+               <?= $user->city && $user->country ?  e($user->city.' - '.$user->country) :''?><br>
+                
+                    <a href="https://www.google.com/maps?q=<?=  e($user->city.' - '.$user->country)?>" target="_blank">Voir sur google Maps</a>
+            </div>
+
+            <div class="col-md-6">
+                <?= !empty($user->twitter)?  '<a href="//twitter.com/"'.$user->twitter.'>@'.e($user->twitter).'</a>'
+                :''?><br>
+                <?= !empty($user->github)?  '<a href="//github.com/"'.$user->github.'>'.e($user->github).'</a>'
+                :''?><br>
+                <?= !empty($user->available_for_hiring)? 'Disponible pour emploi'
+                                                            :'Non disponible pour emploi'?><br>
+            
             </div>
         </div><!--fin de la ligne  -->
+
+        
               
             </div>
             
         </div>
+
+
+        <div class="row"><!--debut de la ligne pour la biogra -->
+            <div class="col-md-12">
+                
+                <h5 class="card-header">Petite Biographie de <?= e($user->name) ?></h5>
+
+                <p>
+                    <?= $user->bio? nl2br(e($user->bio)) :'Aucune biographie pour le moment' ?>
+                </p>
+            </div>
+
+        </div>
+
     </div>
 
 
